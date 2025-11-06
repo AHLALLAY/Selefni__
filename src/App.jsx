b2bf7caad4098de803a48b9442a5fc4894680b79
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-11-06 18:33:42 +0100

    SAL-9 <Installer et configurer React Router>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 function App() {
 
   return (
     <>
-      
+      <main>
+        <h1>App</h1>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
